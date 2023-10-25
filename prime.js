8115cacc07d6e37dad14a1df80e2edca0f305617
VatsalSharma77 <vatsalsharma0217@gmail.com>
2023-10-25 18:50:43 +0530

    updated code

--- prime.js
+++ prime.js
@@ -1,8 +1,11 @@
 function check_prime(num){
     let count = 0;
-    if (num%2==0){
-        count ++;
+    for (let i =0; i<=num;i++){
+        if (num%2==0){
+            count ++;
+        }
     }
+    
     if (count == 2){
         return true;
     }
